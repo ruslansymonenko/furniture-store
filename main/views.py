6a0e66c30758631cd049b308cd8aa5ad6b83906b
ruslansymonenko <symonenkors@gmail.com>
2025-06-20 20:24:25 +0300

    first commit

---
+++ main/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def index(request):
+    context = {
+        'title': 'Store: Home Page',
+    }
+
+    return render(request, 'main/index.html', context)
+
+def about(request):
+    return HttpResponse("About Page")

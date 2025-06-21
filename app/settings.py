96c84c443407a448c26499aa8ac438dfd363c99b
ruslansymonenko <symonenkors@gmail.com>
2025-06-21 15:40:43 +0300

    add static files, changed routing, updated html templates

--- app/settings.py
+++ app/settings.py
@@ -98,6 +98,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
 # Default primary key field type
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- main/views.py
+++ main/views.py
@@ -4,9 +4,16 @@ from django.shortcuts import render
 def index(request):
     context = {
         'title': 'Store: Home Page',
+        'content': 'Furniture Store',
     }
 
-    return render(request, 'main/index.html', context)
+    return render(request, 'main/home.html', context)
 
 def about(request):
-    return HttpResponse("About Page")
+    context = {
+        'title': 'Store: About Page',
+        'content': 'About us',
+        'text_on_page': 'This is furniture store with best products'
+    }
+
+    return render(request, 'main/about.html', context)

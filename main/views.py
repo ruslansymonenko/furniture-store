f456732ba4bf1dee56e63067558077643a6a38a6
ruslansymonenko <symonenkors@gmail.com>
2025-06-22 19:24:08 +0300

    added user template tag for categories, added methods for product

--- main/views.py
+++ main/views.py
@@ -1,14 +1,9 @@
 from django.shortcuts import render
 
-from goods.models import Category
-
 def index(request):
-    categories = Category.objects.all()
-
     context = {
         'title': 'Store: Home Page',
         'content': 'Furniture Store',
-        'categories': categories
     }
 
     return render(request, 'main/home.html', context)

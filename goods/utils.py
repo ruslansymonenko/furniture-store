360f60e7921701bcaa174bcc1df4e30d8fb0bf16
ruslansymonenko <symonenkors@gmail.com>
2025-06-24 20:35:59 +0300

    improved search, css and html changes

--- goods/utils.py
+++ goods/utils.py
@@ -1,16 +1,52 @@
-from django.db.models import Q
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank, SearchHeadline
+from django.db.models import Func, Value, CharField, F
+from django.db.models.query_utils import Q
 
 from goods.models import Product
+
+class Left(Func):
+    function = 'LEFT'
+    output_field = CharField()
 
 def q_search(query):
     if query.isdigit() and len(query) <= 6:
         return Product.objects.filter(id=int(query))
 
-    keywords = [word for word in query.split(' ') if len(word) > 2]
-    q_objects = Q()
+    vector = SearchVector("name", "description")
+    search_query = SearchQuery(query)
 
-    for token in keywords:
-        q_objects |= Q(description__icontains=token)
-        q_objects |= Q(name__icontains=token)
+    results = Product.objects.annotate(
+        short_name=Left(F("name"), Value(25)),
+        short_description=Left(F("description"), Value(50)),
+        rank=SearchRank(vector, search_query)
+    ).filter(rank__gt=0).order_by("-rank")
 
-    return Product.objects.filter(q_objects)
+    results = results.annotate(
+        headline=SearchHeadline(
+            "short_name",
+            search_query,
+            start_sel='<span style="background-color: yellow">',
+            stop_sel="</span>"
+        ),
+    )
+
+    results = results.annotate(
+        bodyline=SearchHeadline(
+            "short_description",
+            search_query,
+            start_sel='<span style="background-color: yellow">',
+            stop_sel="</span>"
+        ),
+    )
+
+    if not results.exists():
+        keywords = [word for word in query.split() if len(word) > 2]
+        q_objects = Q()
+
+        for token in keywords:
+            q_objects |= Q(name__icontains=token)
+            q_objects |= Q(description__icontains=token)
+
+        results = Product.objects.filter(q_objects)
+
+    return results
